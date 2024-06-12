a1a3bddf9bd7d98cc77856019e433488e1cb59ba
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2024-06-12 17:52:51 +0900

    bootstrapの対応

--- resources/views/Admin/havingView.blade.php
+++ resources/views/Admin/havingView.blade.php
@@ -1,33 +1,48 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
+<html>
+
+<body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+<div class="container">
+    <table class="table">
+        <tr>
+            <td>
+                プレイヤー名
+            </td>
+            <td>
+                アイテム名
+            </td>
+            <td>
+                所持個数
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                miura
+            </td>
+            <td>
+                回復薬
+            </td>
+            <td>
+                13
+            </td>
+        </tr>
+    </table>
 </div>
 
-<table>
-    <tr>
-        <td>
-            プレイヤー名
-        </td>
-        <td>
-            アイテム名
-        </td>
-        <td>
-            所持個数
-        </td>
-    </tr>
-
-    <tr>
-        <td>
-            miura
-        </td>
-        <td>
-            回復薬
-        </td>
-        <td>
-            13
-        </td>
-    </tr>
-</table>
 
 <form action="{{url('accounts/index')}}">
-    <input type="submit" value="戻る">
+    <div class="d-flex justify-content-center">
+        <input class="btn btn-secondary" type="submit" value="戻る">
+    </div>
 </form>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
+</body>
+
+
+</html>
--- resources/views/accounts/loginview.blade.php
+++ resources/views/accounts/loginview.blade.php
@@ -1,7 +1,3 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
-</div>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,15 +5,29 @@
 
     <title>Login</title>
 </head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <body>
-<h1>ログイン</h1>
 
-<form method="post" action="{{url('accounts/dologin')}}">
-    @csrf <!--csrf対策-->
-    <p>名前を入力してください<input type="text" name="name"></p>
-    <p>パスワードを入力してください<input type="password" name="password"></p>
-    <input type="submit" value="送信">
-</form>
+<div class="d-flex justify-content-center">
+    <form method="post" action="{{url('accounts/dologin')}}">
+        @csrf <!--csrf対策-->
+        <h1 class="text-center">ログイン</h1>
 
+        <p>名前を入力してください<input type="text" name="name"></p>
+        <p>パスワードを入力してください<input type="password" name="password"></p>
+
+        <div class="d-flex justify-content-center">
+            <input class="btn btn-secondary" type="submit" value="送信">
+        </div>
+
+    </form>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
 </body>
 </html>
